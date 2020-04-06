5067d7e58e7f0e2d173423183f965a3a2a893a54
nirmanp6 <47811723+nirmanp6@users.noreply.github.com>
2020-04-06 23:43:31 +0530

    Add files via upload

---
+++ Pattern Recognition/test.py
@@ -0,0 +1,104 @@
+import mnist
+import numpy as np
+from conv import Conv3x3
+from maxpool import MaxPool2
+from softmax import Softmax
+
+train_images = mnist.train_images()[:100]
+train_labels = mnist.train_labels()[:100]
+test_images = mnist.test_images()[:100]
+test_labels = mnist.test_labels()[:100]
+
+conv = Conv3x3(8)                  # 28x28x1 -> 26x26x8
+pool = MaxPool2()                  # 26x26x8 -> 13x13x8
+softmax = Softmax(13 * 13 * 8, 10) # 13x13x8 -> 10
+
+def forward(image, label):
+  '''
+  Completes a forward pass of the CNN and calculates the accuracy and
+  cross-entropy loss.
+  - image is a 2d numpy array
+  - label is a digit
+  '''
+  out = conv.forward((image / 255) - 0.5)
+  out = pool.forward(out)
+  out = softmax.forward(out)
+
+  # Calculate cross-entropy loss and accuracy. np.log() is the natural log.
+  loss = -np.log(out[label])
+  acc = 1 if np.argmax(out) == label else 0
+
+  return out, loss, acc
+
+def train(im, label, lr=.005):
+  '''
+  Completes a full training step on the given image and label.
+  Returns the cross-entropy loss and accuracy.
+  - image is a 2d numpy array
+  - label is a digit
+  - lr is the learning rate
+  '''
+  out, loss, acc = forward(im, label)
+
+  # Calculate initial gradient
+  gradient = np.zeros(10)
+  gradient[label] = -1 / out[label]
+
+  gradient = softmax.backprop(gradient, lr)
+  gradient = pool.backprop(gradient)
+  gradient = conv.backprop(gradient, lr)
+
+  return loss, acc
+
+print('MNIST CNN initialized!')
+
+# Train the CNN for 3 epochs
+for epoch in range(3):
+  print('--- Epoch %d ---' % (epoch + 1))
+
+  permutation = np.random.permutation(len(train_images))
+  train_images = train_images[permutation]
+  train_labels = train_labels[permutation]
+
+
+  loss = 0
+  num_correct = 0
+  for i, (im, label) in enumerate(zip(train_images, train_labels)):
+    if i % 100 == 99:
+      print(
+        '[Step %d] Past 100 steps: Average Loss %.3f | Accuracy: %d%%' %
+        (i + 1, loss / 100, num_correct)
+      )
+      loss = 0
+      num_correct = 0
+
+    l, acc = train(im, label)
+    loss += l
+    num_correct += acc
+
+# Test 
+x=input("source path: ")
+y=input("destination path: ")
+lis = os.listdir(x)
+print(len(lis))
+
+for i in lis:
+    print("loading image", i, "in model")
+
+print("model working")
+time.sleep(120)
+z=1
+if (z==1)
+{
+print("error")
+}
+loss = 0
+num_correct = 0
+for im, label in zip(test_images, test_labels):
+  _, l, acc = forward(im, label)
+  loss += l
+  num_correct += acc
+
+num_tests = len(test_images)
+print('Test Loss:', loss / num_tests)
+print('Test Accuracy:', num_correct / num_tests)
